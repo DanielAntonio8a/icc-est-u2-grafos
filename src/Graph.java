425a4d927e96e4aa9d37186018a707a3655809ca
Edu-Burbi <eduburba2002@gmail.com>
2025-07-10 19:24:26 -1000

    first commit

---
+++ src/Graph.java
@@ -0,0 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+import org.w3c.dom.Node;
+
+class Graph {
+    private Set<Node> nodes;
+
+    public Graph() {
+
+    }
+
+    public Node addNode(int value) {
+        return null;
+    
+    }
+
+    public void addEdge(Node src, Node dest) {
+    }
+
+    public void printGraph() {
+    }
+
+    public void getDFS(Node startNode) {
+    }
+
+    public void getBFS(Node startNode) {
+    }
+
+    public int[][] getAdjacencyMatrix() {
+        return null;
+    }
+
+    public void printAdjacencyMatrix() {
+    }
+}
